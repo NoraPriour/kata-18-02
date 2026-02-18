fec1f7f39a78d0efd100e0389e1038da0dca6ded
Nora Priour-Petitbon <nora.pp@orange.fr>
2026-02-18 15:03:53 +0100

    ajout function sans boucle

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 let tableau = [
-  { name: "Lina", age: 22, active: true },
-  { name: "Sarah", age: 17, active: true },
-  { name: "Emma", age: 30, active: false }
+    { name: "Lina", age: 22, active: true },
+    { name: "Sarah", age: 17, active: true },
+    { name: "Emma", age: 30, active: false }
 ]
 
-
-
-function getActiveAdult(users) {
+// function avec boucle
+function getActiveAdultBoucle(users) {
     let resultat = []
     users.forEach((elt) => {
         if (elt.age >= 18 && elt.active) {
             resultat.push(elt)
         }
-    }) 
+    })
+    return resultat
+}
 
+console.log(getActiveAdultBoucle(tableau))
+
+// function sans boucle
+function getActiveAdult(users) {
+    let resultat = users.filter((elt) => elt.age >= 18 && elt.active)
     return resultat
 }
 
